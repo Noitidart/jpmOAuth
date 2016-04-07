29e32f04902132410cf16f783a7c1497e6d0593e
Noitidart <noitidart@gmail.com>
2016-04-07 06:00:02 -0700

    rev2
    Updated code but still request is failing

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ Cu.import('resource://gre/modules/Services.jsm');
 Services.scriptloader.loadSubScript(self.data.url('hmac-sha1.js'));
 // console.error('CryptoJS:', CryptoJS.HmacSHA1("rawr", "11").toString(CryptoJS.enc.Hex));
 
+// start - common functions
 // rev1 - https://gist.github.com/Noitidart/6c172e77fe48f78521f2
 var jQLike = { // my stand alone jquery like functions
     serialize: function(aSerializeObject) {
@@ -18,6 +19,65 @@ var jQLike = { // my stand alone jquery like functions
         return serializedStrArr.join('&');
     }
 };
+
+// rev1 - https://gist.github.com/Noitidart/c4ab4ca10ff5861c720b
+function validateOptionsObj(aOptions, aOptionsDefaults) {
+	// ensures no invalid keys are found in aOptions, any key found in aOptions not having a key in aOptionsDefaults causes throw new Error as invalid option
+	for (var aOptKey in aOptions) {
+		if (!(aOptKey in aOptionsDefaults)) {
+			console.error('aOptKey of ' + aOptKey + ' is an invalid key, as it has no default value, aOptionsDefaults:', aOptionsDefaults, 'aOptions:', aOptions);
+			throw new Error('aOptKey of ' + aOptKey + ' is an invalid key, as it has no default value');
+		}
+	}
+	
+	// if a key is not found in aOptions, but is found in aOptionsDefaults, it sets the key in aOptions to the default value
+	for (var aOptKey in aOptionsDefaults) {
+		if (!(aOptKey in aOptions)) {
+			aOptions[aOptKey] = aOptionsDefaults[aOptKey];
+		}
+	}
+}
+
+function spliceObj(obj1, obj2) {
+	/**
+	 * By reference. Adds all of obj2 keys to obj1. Overwriting any old values in obj1.
+	 * Was previously called `usurpObjWithObj`
+	 * @param obj1
+	 * @param obj2
+	 * @returns undefined
+	 */
+	for (var attrname in obj2) { obj1[attrname] = obj2[attrname]; }
+}
+function overwriteObjWithObj(obj1, obj2){
+	/**
+	 * No by reference. Creates a new object. With all the keys/values from obj2. Adds in the keys/values that are in obj1 that were not in obj2.
+	 * @param obj1
+	 * @param obj2
+	 * @returns obj3 a new object based on obj1 and obj2
+	 */
+
+    var obj3 = {};
+    for (var attrname in obj1) { obj3[attrname] = obj1[attrname]; }
+    for (var attrname in obj2) { obj3[attrname] = obj2[attrname]; }
+    return obj3;
+}
+
+function alphaStrOfObj(aObj, aParseFunc, aJoinStr, aDblQuot) {	
+	var arr = Object.keys(aObj);
+	arr.sort();
+	
+	if (!aParseFunc) {
+		aParseFunc = function(aToBeParsed) {
+			return aToBeParsed;
+		};
+	}
+	
+	for (var i=0; i<arr.length; i++) {
+		arr[i] = aParseFunc(arr[i]) + '=' + (aDblQuot ? '"' : '') + aParseFunc(aObj[arr[i]]) + (aDblQuot ? '"' : '');
+	}
+	
+	return arr.join(aJoinStr);
+}
 
 function to_rfc3986(aStr) {
 	// https://af-design.com/2008/03/14/rfc-3986-compliant-uri-encoding-in-javascript/
@@ -40,10 +100,19 @@ function nonce(length) {
     }
     return text;
 }
+// end - common functions
 
-function twitterRequestDetails(aURL, aMethod, aRawPostDataObj={}, aConsumerKey, aOauthTokenSecret=null) {
+function twitterRequestDetails(aURL, aMethod, aConsumerKey, aOptions) {
 	// generates a twitter signature
 	// rawPostDataObj is the post data non url encoded/escpaed
+	
+	var aOptionsDefaults = {
+		postData: null, // object
+		oauthTokenSecret: null, // string
+		extraOauthParams: null // object
+	};
+	
+	validateOptionsObj(aOptions, aOptionsDefaults);
 	
 	var oauth_params = {
 		oauth_nonce: nonce(42),
@@ -54,44 +123,29 @@ function twitterRequestDetails(aURL, aMethod, aRawPostDataObj={}, aConsumerKey, 
 		oauth_version: '1.0'
 		// oauth_signature: '???' // added in later after sig is created
 	};
-	if (aOauthTokenSecret) {
-		oauth_params.oauth_token = aOauthTokenSecret;
+	if (aOptions.oauthTokenSecret) {
+		oauth_params.oauth_token = aOptions.oauthTokenSecret;
+	}
+	
+	if (aOptions.extraOauthParams) {
+		spliceObj(oauth_params, aOptions.extraOauthParams);
 	}
 	
 	// for signautre reasons twitter docs say "Sort the list of parameters alphabetically[1] by encoded key[2]." here - https://dev.twitter.com/oauth/overview/creating-signatures
 	// create oauth_signature
-	var sig_arr = [];
-	for (var cOauthParam in oauth_params) {
-		if (cOauthParam == 'oauth_signature') {
-			continue;
-		}
-		sig_arr.push([to_rfc3986(cOauthParam), to_rfc3986(oauth_params[cOauthParam])]);
+	var sig_collection;
+	if (aOptions.postData) {
+		sig_collection = overwriteObjWithObj(oauth_params, aOptions.postData);
+	} else {
+		sig_collection = oauth_params;
 	}
-	for (var cRawPostEntry in aRawPostDataObj) {
-		sig_arr.push([to_rfc3986(cRawPostEntry), to_rfc3986(aRawPostDataObj[cRawPostEntry])]);
-	}
-	sig_arr.sort(function(a, b) {
-		return a[0].localeCompare(b[0]);
-	});
+	console.error('sig_collection:', sig_collection);
 	
-	console.error('sig_arr:', sig_arr);
-	
-	var sig_str = [];
-	for (var i=0; i<sig_arr.length; i++) {
-		sig_str.push(sig_arr[i][0] + '=' + sig_arr[i][1] + '');
-	}
-	sig_str = sig_str.join('&');
-	
-	sig_str = aMethod.toUpperCase() + '&' + to_rfc3986(aURL) + '&' + to_rfc3986(sig_str);
-	
+	var sig_str = aMethod.toUpperCase() + '&' + to_rfc3986(aURL) + '&' + to_rfc3986(alphaStrOfObj(sig_collection, to_rfc3986, '&'));
 	console.error('sig_str:', sig_str);
 	
 	// create signing key
-	var sig_key = to_rfc3986(aConsumerKey) + '&';
-	
-	if (aOauthTokenSecret) {
-		sig_key += to_rfc3986(aOauthTokenSecret);
-	}
+	var sig_key = to_rfc3986(aConsumerKey) + '&' + (aOptions.oauthTokenSecret ? to_rfc3986(aOptions.oauthTokenSecret) : '');
 	console.error('sig_key:', sig_key);
 	
 	// create encoded sig
@@ -99,50 +153,37 @@ function twitterRequestDetails(aURL, aMethod, aRawPostDataObj={}, aConsumerKey, 
 	oauth_params.oauth_signature = sig_hmac_sha1_base64;
 	
 	// create header_auth_str
-	var header_auth = [];
-	for (var cOauthParam in oauth_params) {
-		header_auth.push([cOauthParam, oauth_params[cOauthParam]]);
-	}
-	
-	header_auth.sort(function(a, b) {
-		return a[0].localeCompare(b[0]);
-	});
-	console.error('header_auth:', header_auth);
-	
-	var header_auth_str = [];
-	for (var i=0; i<header_auth.length; i++) {
-		header_auth_str.push(to_rfc3986(header_auth[i][0]) + '="' + to_rfc3986(header_auth[i][1]) + '"');
-	}
-	header_auth_str = header_auth_str.join(', ');
-	// console.error('header_auth_str:', header_auth_str);
+	var header_auth_str = alphaStrOfObj(oauth_params, to_rfc3986, ', ', true);
+	console.error('header_auth_str:', header_auth_str);
 	
 	return {
 		url: aURL,
+		method: aMethod,
 		header_auth: 'OAuth ' + header_auth_str,
-		postDataStrSerialized: jQLike.serialize(aRawPostDataObj)
+		postDataStr: aOptions.postData ? jQLike.serialize(aOptions.postData) : null // this is a serialized version of the aOptions.postData object
 	};
 }
 
-var cReqDetail = twitterRequestDetails('https://api.twitter.com/oauth/request_token', 'POST', {
-  oauth_callback: 'http://127.0.0.1/floppers'
-}, 'AjONvgAdbD8YWCtRn5U9yA', null);
+var cReqDetail = twitterRequestDetails('https://api.twitter.com/oauth/request_token', 'POST', 'AjONvgAdbD8YWCtRn5U9yA', {
+	extraOauthParams: {
+		oauth_callback: 'http://127.0.0.1/floppers'
+	}
+});
 console.error('cReqDetail:', cReqDetail);
 
-var req = {
+
+Request({
 	url: cReqDetail.url,
-	content: cReqDetail.postDataStrSerialized,
 	headers: {
 		Authorization: cReqDetail.header_auth
 	},
 	onComplete: function (response) {
 		console.error(response.text, response.json);
 		
-	// tabs.open('https://api.twitter.com/oauth2/token?' + jQLike.serialize({
-		// oauth_token: response.json.oauth_token,
-	// });
+		// tabs.open('https://api.twitter.com/oauth2/token?' + jQLike.serialize({
+			// oauth_token: response.json.oauth_token,
+		// });
 	}
-};
-console.error('req:', req);
-Request(req).post();
+})[cReqDetail.method.toLowerCase()]();
 
 console.error('ok did req');
